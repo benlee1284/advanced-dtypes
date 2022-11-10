37b07e6add419c3bb727b668de15e62870880a6a
benlee1284 <ben.lee@kayenta.io>
2022-11-10 10:06:07 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="advanced-dtypes",
+    version="0.0.1",
+    author="Ben Lee",
+    description="Data types not found in the standard library that are cool in various different ways ;)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    py_modules=["advanced_dtypes"],
+    package_dir={"":"."},
+    install_requires=[],
+)
